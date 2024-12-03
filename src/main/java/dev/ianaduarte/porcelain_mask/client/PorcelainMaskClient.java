7809023c4faffea0afcb6a084f3492d01ea7119a
ianaduarte <118121781+ianaduarte@users.noreply.github.com>
2024-12-03 15:17:28 -0400

    missed a few spots

--- src/main/java/dev/ianaduarte/porcelain_mask/client/PorcelainMaskClient.java
+++ src/main/java/dev/ianaduarte/porcelain_mask/client/PorcelainMaskClient.java
@@ -1,11 +1,13 @@
-package one.ianthe.porcelain_mask.client;
+package dev.ianaduarte.porcelain_mask.client;
 
 
+import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.model.loading.v1.ModelLoadingPlugin;
 
 
-public class PorcelainMaskClient{
-	public void initialize() {
+public class PorcelainMaskClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
 		ModelLoadingPlugin.register(new SpecialModelLoadingPlugin());
 	}
 }
--- src/main/java/dev/ianaduarte/porcelain_mask/client/SpecialModelLoadingPlugin.java
+++ src/main/java/dev/ianaduarte/porcelain_mask/client/SpecialModelLoadingPlugin.java
@@ -1,10 +1,10 @@
-package one.ianthe.porcelain_mask.client;
+package dev.ianaduarte.porcelain_mask.client;
 
 import net.fabricmc.fabric.api.client.model.loading.v1.ModelLoadingPlugin;
 import net.minecraft.client.resources.model.ModelResourceLocation;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.packs.resources.ResourceManager;
-import one.ianthe.porcelain_mask.registry.SpecialModelRegistry;
+import dev.ianaduarte.porcelain_mask.registry.SpecialModelRegistry;
 
 import java.util.function.Consumer;
 
